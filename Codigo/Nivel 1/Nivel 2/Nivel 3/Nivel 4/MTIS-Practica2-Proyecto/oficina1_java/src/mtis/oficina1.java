dc76496c740d7943d53837781ab1d09786ff1773
Cristian Andrés Córdoba Silvestre <cacs2@alu.ua.es>
2025-03-06 15:07:10 +0100

    mas avances

--- Codigo/Nivel 1/Nivel 2/Nivel 3/Nivel 4/MTIS-Practica2-Proyecto/oficina1_java/src/mtis/oficina1.java
+++ Codigo/Nivel 1/Nivel 2/Nivel 3/Nivel 4/MTIS-Practica2-Proyecto/oficina1_java/src/mtis/oficina1.java
@@ -10,6 +10,31 @@ import javax.jms.TextMessage;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 
-public class oficina1 {
+public class Oficina1 {
+
+    public static void main(String []args) throws JMSException {
+        // URL of the JMS server.
+		String url = "tcp://localhost:61616";
+
+        // Getting JMS connection from the server and starting it
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
+        Connection connection = connectionFactory.createConnection();
+        connection.start();
+
+        // JMS messages are sent and received using a Session. We will
+        // create here a non-transactional session object. If you want
+        // to use transactions you should set the first parameter to 'true'
+        Session session = connection.createSession(false,
+            Session.AUTO_ACKNOWLEDGE);
+
+        Destination lecturas_temperaturas_oficina1_destination = session.createTopic("lecturas_temperaturas_oficina1");
+
+        Destination actuador_temperatura_oficina1_destination = session.createTopic("actuador_temperatura_oficina1");
+
+
+        MessageProducer lecturas_temperaturas_oficina1_producer = session.createProducer(lecturas_temperaturas_oficina1_destination);
+
+        MessageProducer actuador_temperatura_oficina1_consumer = session.createConsumer(actuador_temperatura_oficina1_destination);
+    }
     
 }
