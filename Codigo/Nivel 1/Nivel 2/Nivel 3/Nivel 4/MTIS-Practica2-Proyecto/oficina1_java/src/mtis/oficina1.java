d1a2122f8b84e5319f058b3340ebd533d32e77ca
Cristian Andrés Córdoba Silvestre <cacs2@alu.ua.es>
2025-03-06 23:49:02 +0100

    Oficina1.java refactorizada

--- Codigo/Nivel 1/Nivel 2/Nivel 3/Nivel 4/MTIS-Practica2-Proyecto/oficina1_java/src/mtis/oficina1.java
+++ Codigo/Nivel 1/Nivel 2/Nivel 3/Nivel 4/MTIS-Practica2-Proyecto/oficina1_java/src/mtis/oficina1.java
@@ -5,69 +5,108 @@ import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
 import javax.jms.JMSException;
-import javax.jms.Message;
 import javax.jms.MessageConsumer;
-import javax.jms.MessageListener;
 import javax.jms.MessageProducer;
 import javax.jms.Session;
 import javax.jms.TextMessage;
+import javax.jms.Message;
+import javax.jms.MessageListener;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
 public class Oficina1 implements MessageListener {
 
-    // Atributo privado para almacenar la temperatura
+    // Private attribute to store the temperature
     private int temperature;
 
     public Oficina1() {
-        this.temperature = 0; // Valor inicial
+        this.temperature = 0; // Initial value
     }
-
-    // Método que inicializa la conexión JMS y envía mensajes periódicamente
-    public void start() throws JMSException {
+    
+    /**
+     * Helper inner class to hold the JMS components.
+     */
+    private static class JMSComponents {
+        public Connection connection;
+        public Session session;
+        public MessageProducer producer;
+        public MessageConsumer consumer;
+        
+        public JMSComponents(Connection connection, Session session, MessageProducer producer, MessageConsumer consumer) {
+            this.connection = connection;
+            this.session = session;
+            this.producer = producer;
+            this.consumer = consumer;
+        }
+    }
+    
+    /**
+     * Sets up the JMS connection, session, destinations, producer, and consumer.
+     * The consumer's message listener is set to this instance.
+     */
+    private JMSComponents setupJMS() throws JMSException {
         System.out.println("ComienzOOOo");
         String url = "tcp://localhost:61616";
-
+        
         CountDownLatch latch = new CountDownLatch(1);
-
-        // Crear conexión y sesión
+        
+        // Create connection and session
         ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
         Connection connection = connectionFactory.createConnection();
         connection.start();
         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-
-        // Crear destinos
-        Destination lecturas_dest = session.createTopic("lecturas_temperaturas_oficina1");
-        Destination actuador_dest = session.createTopic("actuador_temperatura_oficina1");
-
-        // Crear productor y consumidor
-        MessageProducer producer = session.createProducer(lecturas_dest);
-        MessageConsumer consumer = session.createConsumer(actuador_dest);
+        
+        // Create destinations
+        Destination lecturasDest = session.createTopic("lecturas_temperaturas_oficina1");
+        Destination actuadorDest = session.createTopic("actuador_temperatura_oficina1");
+        
+        // Create producer and consumer
+        MessageProducer producer = session.createProducer(lecturasDest);
+        MessageConsumer consumer = session.createConsumer(actuadorDest);
         consumer.setMessageListener(this);
-
-        // Bucle para enviar mensajes periódicamente
+        
+        return new JMSComponents(connection, session, producer, consumer);
+    }
+    
+    /**
+     * This method updates the temperature attribute using Utils,
+     * builds the JSON payload, creates a TextMessage, sends it through the producer,
+     * and then pauses for 4000 milliseconds.
+     */
+    private void sendTemperatureMessage(Session session, MessageProducer producer) throws JMSException, InterruptedException {
+        // Update the 'temperature' attribute using Utils
+        this.temperature = Utils.manejarTemperaturaRandomIndicator();
+        
+        // Build the JSON payload with the desired format
+        String jsonPayload = "{"
+                + "\"temperature\": " + this.temperature + ","
+                + "\"cold_system_activated\": false,"
+                + "\"heat_system_activated\": false"
+                + "}";
+        
+        // Create the TextMessage with the JSON payload and send it
+        TextMessage message = session.createTextMessage(jsonPayload);
+        producer.send(message);
+        
+        // Pause for 4000 milliseconds
+        Thread.sleep(4000);
+    }
+    
+    /**
+     * Initializes the JMS connection and periodically sends messages.
+     */
+    public void start() throws JMSException {
+        JMSComponents jmsComponents = setupJMS();
+        
+        // Loop to periodically send messages
         while (true) {
             try {
-                // Actualiza el atributo 'temperature' con el valor obtenido de Utils
-                this.temperature = Utils.manejarTemperaturaRandomIndicator();
-
-                // Construir el JSON a enviar con el formato deseado
-                String jsonPayload = "{"
-                        + "\"temperature\": " + this.temperature + ","
-                        + "\"cold_system_activated\": false,"
-                        + "\"heat_system_activated\": false"
-                        + "}";
-                
-                // Crear el mensaje con el JSON
-                TextMessage message = session.createTextMessage(jsonPayload);
-                producer.send(message);
-
-                Thread.sleep(4000);
+                sendTemperatureMessage(jmsComponents.session, jmsComponents.producer);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
     }
-
+    
     @Override
     public void onMessage(Message message) {
         try {
@@ -81,7 +120,7 @@ public class Oficina1 implements MessageListener {
             System.out.println("Got a JMS Exception!");
         }
     }
-
+    
     public static void main(String[] args) throws JMSException {
         Oficina1 oficina = new Oficina1();
         oficina.start();
