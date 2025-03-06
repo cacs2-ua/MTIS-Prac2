02a90a4d1876e14a06892badab379edbf866d92b
Cristian Andrés Córdoba Silvestre <cacs2@alu.ua.es>
2025-03-06 14:42:15 +0100

    ......

--- Codigo/Nivel 1/Nivel 2/Nivel 3/Nivel 4/MTIS-Practica2-Proyecto/oficina1_java/src/mtis/oficina1.java
+++ Codigo/Nivel 1/Nivel 2/Nivel 3/Nivel 4/MTIS-Practica2-Proyecto/oficina1_java/src/mtis/oficina1.java
@@ -1,5 +1,15 @@
 package mtis;
 
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
 public class oficina1 {
     
 }
