20339e290bbf1800726f7c5c2b86f070c314e88e
Cristian Andrés Córdoba Silvestre <cacs2@alu.ua.es>
2025-03-06 20:20:47 +0100

    valores random de temperatura ok

--- Codigo/Nivel 1/Nivel 2/Nivel 3/Nivel 4/MTIS-Practica2-Proyecto/oficina1_java/src/mtis/oficina1.java
+++ Codigo/Nivel 1/Nivel 2/Nivel 3/Nivel 4/MTIS-Practica2-Proyecto/oficina1_java/src/mtis/oficina1.java
@@ -42,11 +42,23 @@ public class Oficina1 implements MessageListener {
         actuador_temperatura_oficina1_consumer.setMessageListener(new Oficina1());
 
         Random random = new Random();
+        int temperaturaRandomIndicator = 0;
+        int randomNumber = 0;
         while (true) {
             try {
-                int randomNumber = random.nextInt(46); // 0 a 45 (inclusive)
+                temperaturaRandomIndicator = temperaturaRandomIndicator % 3;
+
+                if (temperaturaRandomIndicator == 0) {
+                    randomNumber = random.nextInt(16) + 15; // 15 a 30 (ambos inclusive)
+                } else if (temperaturaRandomIndicator == 1) {
+                    randomNumber = random.nextInt(15); // 15 a 30 (ambos inclusive)
+                } else {
+                    randomNumber = random.nextInt(15) + 31; // 30 a 45 (ambos inclusive)
+                }
                 TextMessage message = session.createTextMessage(String.valueOf(randomNumber));
                 lecturas_temperaturas_oficina1_producer.send(message);
+                temperaturaRandomIndicator++;
+
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
